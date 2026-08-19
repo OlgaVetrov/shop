56a2202acd40d98445c0ac06a44f10bc927e700c
OlgaVetrov <otyakovskayaolga@gmail.com>
2026-08-19 17:37:57 -0400

    Formated file

--- src/About.jsx
+++ src/About.jsx
@@ -2,22 +2,24 @@ import Navbar from "./Components/Navbar";
 import Text from "./Text";
 
 const About = () => {
-return (
-<div className="info-page">
-<Navbar />
+  return (
+    <div className="info-page">
+      <Navbar />
 
-<main className="info-content">
-<h1>About Us</h1>
+      <main className="info-content">
+        <h1>About Us</h1>
 
-<Text />
+        <Text />
 
-<div className="about-image">
-<img src="https://images.pexels.com/photos/36730629/pexels-photo-36730629.jpeg"
-alt="Fashion store"/>
-</div>
-</main>
-</div>
-);
+        <div className="about-image">
+          <img
+            src="https://images.pexels.com/photos/36730629/pexels-photo-36730629.jpeg"
+            alt="Fashion store"
+          />
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default About;
