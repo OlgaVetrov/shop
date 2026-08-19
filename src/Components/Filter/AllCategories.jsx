50b60c44d19b376df794d33fedb79c81bb999872
OlgaVetrov <otyakovskayaolga@gmail.com>
2026-08-19 18:39:12 -0400

    Updated project

--- src/Components/Filter/AllCategories.jsx
+++ src/Components/Filter/AllCategories.jsx
@@ -1,9 +1,21 @@
+import { useDispatch, useSelector } from "react-redux";
+import { filterClothes } from "../../redux/clothesSlice";
+
 const categories = ["dress", "pants", "skirt", "shoes", "shirt"];
 
 const AllCategories = () => {
+  const dispatch = useDispatch();
+  const selectedCategory = useSelector(
+    (state) => state.clothes.selectedCategory,
+  );
+
   return (
     <div className="category-container">
-      <button className="category-button active">All Categories</button>
+      <button
+        className={
+          selectedCategory === "all" ? "category-button active" : "category-button"}
+        onClick={() => dispatch(filterClothes("all"))}
+      > All Categories </button>
     </div>
   );
 };
